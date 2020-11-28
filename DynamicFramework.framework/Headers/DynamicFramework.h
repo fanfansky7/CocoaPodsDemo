3dd90e4ecb3e28203a7cf930aedcc3abc409f4a2
fanfanhome <364549495@qq.com>
2020-11-28 16:12:40 +0800

    添加podspec 和 framework

---
+++ DynamicFramework.framework/Headers/DynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  DynamicFramework.h
+//  DynamicFramework
+//
+//  Created by 范孟磊 on 2020/11/28.
+//  Copyright © 2020 范. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicFramework.
+FOUNDATION_EXPORT double DynamicFrameworkVersionNumber;
+
+//! Project version string for DynamicFramework.
+FOUNDATION_EXPORT const unsigned char DynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicFramework/PublicHeader.h>
+
+#import "Ftest.h"
---
+++ DynamicFramework.framework/Headers/Ftest.h
@@ -0,0 +1,17 @@
+//
+//  Ftest.h
+//  DynamicFramework
+//
+//  Created by 范孟磊 on 2020/11/28.
+//  Copyright © 2020 范. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Ftest : NSObject
++(void)Log;
+@end
+
+NS_ASSUME_NONNULL_END
